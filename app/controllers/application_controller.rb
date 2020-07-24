f88f69f8b58285777c90e59f6ee330cea21fe942
mspcity <codepark2019@gmail.com>
2020-07-24 03:31:32 -0400

    make some routes for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,53 @@ class ApplicationController < Sinatra::Base
     erb :main
   end
 
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/schedules'
+    else
+      erb :'users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username].blank?
+      redirect to '/signup'
+    elsif params[:password].blank?
+      redirect to '/signup'
+    else
+      @user = User.create(username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+
+      redirect to 'schedules/new'
+    end
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/schedules'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && user.authenticate(params[:params[:password])
+      session[:user_id] = @user.id
+
+      redirect to '/schedules'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+
+    redirect to '/login'
+  end
+
+
+
 end
