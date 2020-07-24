7960b8fbc379d6a252b8e77895421b2b332eeb44
mspcity <codepark2019@gmail.com>
2020-07-24 02:45:07 -0400

    created views/main.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessioins
+    set:session_secret, "secret"
   end
 
   get "/" do
-    erb :welcome
+    erb :main
   end
 
 end
